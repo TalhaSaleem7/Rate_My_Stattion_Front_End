98ee8937b990d9822b43077ebcc26e328d5ca765
Ahtisham hussain <147423967+ahteshamniqosa@users.noreply.github.com>
2024-06-07 09:13:06 -0700

    ////ahtisham

---
+++ src/Filter/Filter.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import kabc from '../img/kabc.png'
+
+
+const Filter = ()=>{
+
+
+return(
+
+
+
+    <div>
+                    <div class="job-ah-post">
+                        <h2>My Ratings<span> (19)</span></h2>
+
+
+                        <div class="job-ah-Filter">
+                            <div class="job-ah-Filter-card">
+                                <p>Filter:</p>
+                                <select name="cars" id="cars">
+                                    <option value="" disabled="" selected="" hidden="">Job type</option>
+                                    
+                                    <option value="option2">option1</option>
+                                    <option value="option3">option2</option>
+                                    <option value="option4">option3</option>
+                                  </select>
+                            </div>
+
+
+                            <div class="job-ah-Filter-card">
+                                <p>Short:</p>
+                                <select name="cars" id="cars">
+                                    <option value="" disabled="" selected="" hidden="">Newest</option>
+                                   
+                                    <option value="option2">option1</option>
+                                    <option value="option3">option2</option>
+                                    <option value="option4">option3</option>
+                                  </select>
+                            </div>
+                        </div>
+                    </div>
+
+    </div>
+    
+)
+
+
+
+
+}
+
+
+
+export default Filter
