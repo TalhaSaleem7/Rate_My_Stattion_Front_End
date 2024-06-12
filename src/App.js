b92205d4024311918dc7d51e6b403d6cb9aff80c
Ahtisham hussain <147423967+ahteshamniqosa@users.noreply.github.com>
2024-06-12 10:38:55 -0700

    /////ahtsiham

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Reportersd from "./reportersd/Reportersd";
 import { Homepage } from "./Home/Homepage";
 import About from "./About/About";
 import Newsletter from "./Newsletter/Newsletter";
-
 import Dirctors from "./Dirctors/Dirctors";
 import Award from "./Award/Award";
 import Openings from "./Openings/Openings";
@@ -38,20 +37,7 @@ import AwardForm from "./Popups/Station_profile_popups/Award_h";
 import JobOpeningForm from "./Popups/Station_profile_popups/Jobopening_h";
 import SubmitNewsDirectorForm from "./Popups/Submitpopup_h.js/SubmitDirector_h";
 import ApplyForm from "./Popups/Submitpopup_h.js/Apply_h";
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { Provider } from 'react-redux'; // Import Provider from react-redux
-
-
-// import '../src/css/App.css';
-
-// import Header from './Header/Header';
-// import Reportersd from './reportersd/Reportersd';
-// import { Homepage } from './Home/Homepage';
-// import About from './About/About';
-// import Newsletter from './Newsletter/Newsletter';
-// import NewsletterArticle from './NewsletterArticle/NewsletterArticle';
+import NewsletterArticle from './NewsletterArticle/NewsletterArticle';
 import Viewjob from './Viewjob/Viewjob';
 import Shop from './Shop/Shop';
 import Viewjobdetail from './Viewjobdetail/Viewjobdetail';
@@ -60,62 +46,10 @@ import Jobopening from './Jobopening/Jobopening';
 import StationSearchResult from './StationSearchResult/StationSearchResult';
 import DirectorSearchResult from './DirectorSearchResult/DirectorSearchResult';
 import MyCartSD from './MyCartSD/MyCartSD';
-
-
-
- 
-// import Dirctors from './Dirctors/Dirctors';
-// import Award from './Award/Award';
-// import Openings from './Openings/Openings';
-// import Cardah from './Cardah/Cardah';
-// import KABC from './KABC/KABC';
-// import KABC2 from './KABC/KABC2';
-// import About2 from './About/About2';
-// import Dirctors2 from './Dirctors/Dirctors2';
-// import Award2 from './Award/Award2';
-// import Openings2 from './Openings/Openings2';
-// import Station from './Station/Station';
-// import Filter from './Filter/Filter';
 import EmployerLoginPg from './EmployerLoginPg/EmployerLoginPg';
 import EmployerRegisterPage from './EmployerRegisterPage/EmployerRegisterPage';
 import SubcriptionPage from './SubcriptionPage/SubcriptionPage';
 import SubmitAStationForm from './SubmitAStationForm/SubmitAStationForm';
-
-
-
-
-
-
-
-
-// import Header from "./Header/Header";
-// import Reportersd from "./reportersd/Reportersd";
-// import { Homepage } from "./Home/Homepage";
-// import About from "./About/About";
-// import Newsletter from "./Newsletter/Newsletter";
-// import NewsletterArticle from "./NewsletterArticle/NewsletterArticle";
-// import Viewjob from "./Viewjob/Viewjob";
-// import Shop from "./Shop/Shop";
-// import Viewjobdetail from "./Viewjobdetail/Viewjobdetail";
-// import Contactus from "./Contactus/Contactus";
-// import Jobopening from "./Jobopening/Jobopening";
-// import StationSearchResult from "./StationSearchResult/StationSearchResult";
-// import DirectorSearchResult from "./DirectorSearchResult/DirectorSearchResult";
-// import MyCartSD from "./MyCartSD/MyCartSD";
-// import EmployerLoginPG from "./EmployerLoginPG/EmployerLoginPG";
-
-// import Dirctors from "./Dirctors/Dirctors";
-// import Award from "./Award/Award";
-// import Openings from "./Openings/Openings";
-// import Cardah from "./Cardah/Cardah";
-// import KABC from "./KABC/KABC";
-// import KABC2 from "./KABC/KABC2";
-// import About2 from "./About/About2";
-// import Dirctors2 from "./Dirctors/Dirctors2";
-// import Award2 from "./Award/Award2";
-// import Openings2 from "./Openings/Openings2";
-// import Station from "./Station/Station";
-// import Filter from "./Filter/Filter";
 import About1 from "./About/About1";
 import Reportersd2 from "./reportersd/Reportersd2";
 import Chartah1 from "./Chart/Chartah1";
@@ -138,20 +72,20 @@ import Buyerah from "./Buyerah/Buyerah";
 import Cooperah from "./Cooperah/Cooperah";
 import Cooperah1 from "./Cooperah/Cooperah1";
 
+
+
 function App() {
   return (
   
       <Router>
         <Routes>
-
+        <Route path="/" exact element={<Homepage/>} />
         <Route path="/kabc" exact element={<KABC />} />
         <Route path="/kabc2" exact element={<KABC2 />} />
         <Route path="/kabcah" exact element={<KABCah />} />
         <Route path="/kabcah1" exact element={<KABCah1 />} />
         <Route path="/Header" exact element={<Header />} />
-        {/* <Route path="/About" exact element={<About />} /> */}
         <Route path="/Footerah" exact element={<Footerah />} />
-
         <Route path="/About" exact element={<About />} />
         <Route path="/About1" exact element={<About1 />} />
         <Route path="/About2" exact element={<About2 />} />
@@ -166,14 +100,10 @@ function App() {
         <Route path="/Cardah" exact element={<Cardah />} />
         <Route path="/Station" exact element={<Station />} />
         <Route path="/Filter" exact element={<Filter />} />
-
- 
-  
         <Route path="/reportersd" exact element={<Reportersd/>} />
-        <Route path="/" exact element={<Homepage/>} />
         <Route path="/about" exact element={<About/>} />
         <Route path="/newsletter" exact element={<Newsletter/>} />
-        {/* <Route path="/newsletterarticle" exact element={<NewsletterArticle/>} /> */}
+        <Route path="/newsletterarticle" exact element={<NewsletterArticle/>} />
         <Route path="/viewjob" exact element={<Viewjob/>} />
         <Route path="/viewjobdetail" exact element={<Viewjobdetail/>} />
         <Route path="/shop" exact element={<Shop/>} />
@@ -186,22 +116,15 @@ function App() {
         <Route path="/employerregister" exact element={<EmployerRegisterPage/>} />
         <Route path="/subscription" exact element={<SubcriptionPage/>} />
         <Route path="/submitstaionform" exact element={<SubmitAStationForm/>} />
-  
-   
         <Route path="/reportersd" exact element={<Reportersd />} />
         <Route path="/home" exact element={<Homepage />} />
         <Route path="/about" exact element={<About />} />
         <Route path="/newsletter" exact element={<Newsletter />} />
-        {/* hamza routes */}
         <Route path="/accountsettingh" exact element={<AccountSettings />} />
         <Route path="/faqmainh" exact element={<FAQPage />} />
         <Route path="/faqdetailh" exact element={<FaqDetail />} />
         <Route path="/faqsearchresulth" exact element={<FaqSearchresult />} />
-        <Route
-          path="/termandconditionh"
-          exact
-          element={<TermsAndConditions />}
-        />
+        <Route path="/termandconditionh" exact element={<TermsAndConditions />}/>
         <Route path="/workforush" exact element={<Workforus />} />
         <Route path="/abouth" exact element={<Abouth />} />
         <Route path="/experienceh" exact element={<ExperienceForm />} />
@@ -211,13 +134,8 @@ function App() {
         <Route path="/newsdirectorh" exact element={<NewsDirectorForm />} />
         <Route path="/awardh" exact element={<AwardForm />} />
         <Route path="/jobopeningh" exact element={<JobOpeningForm />} />
-        <Route
-          path="/submitnewsdirectorh"
-          exact
-          element={<SubmitNewsDirectorForm />}
-        />
+        <Route path="/submitnewsdirectorh" exact element={<SubmitNewsDirectorForm />}/>
         <Route path="/applyformh" exact element={<ApplyForm />} />
-
         <Route path="/Chartah1" exact element={<Chartah1 />} />
         <Route path="/Chartah2" exact element={<Chartah2 />} />
         <Route path="/Chartah3" exact element={<Chartah3 />} />
@@ -231,34 +149,20 @@ function App() {
         <Route path="/Buyerah" exact element={<Buyerah />} />
         <Route path="/Cooperah" exact element={<Cooperah />} />
         <Route path="/Cooperah1" exact element={<Cooperah1 />} />
-
         <Route path="/reportersd" exact element={<Reportersd />} />
         <Route path="/reportersd2" exact element={<Reportersd2 />} />
         <Route path="/home" exact element={<Homepage />} />
         <Route path="/about" exact element={<About />} />
         <Route path="/newsletter" exact element={<Newsletter />} />
-        {/* <Route
-          path="/newsletterarticle"
-          exact
-          element={<NewsletterArticle />}
-        /> */}
+        <Route path="/newsletterarticle" exact  element={<NewsletterArticle />}/>
         <Route path="/viewjob" exact element={<Viewjob />} />
         <Route path="/viewjobdetail" exact element={<Viewjobdetail />} />
         <Route path="/shop" exact element={<Shop />} />
         <Route path="/contactus" exact element={<Contactus />} />
         <Route path="/jobopening" exact element={<Jobopening />} />
-        <Route
-          path="/StationSearchResult"
-          exact
-          element={<StationSearchResult />}
-        />
-        <Route
-          path="/DirectorSearchResult"
-          exact
-          element={<DirectorSearchResult />}
-        />
-        <Route path="/mycart" exact element={<MyCartSD />} />
-        {/* <Route path="/employerlogin" exact element={<EmployerLoginPG />} /> */}
+        <Route path="/StationSearchResult" exact  element={<StationSearchResult />}/>
+        <Route path="/DirectorSearchResult" exact element={<DirectorSearchResult />}/>
+
       </Routes>
     </Router>
   );
