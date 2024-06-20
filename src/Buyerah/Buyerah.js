5188f85d9866b85d8718437356ed5611c6d32d57
hamzasid19 <156464640+hamzasid19@users.noreply.github.com>
2024-06-20 07:57:16 -0700

    //hamzasweatresponse

--- src/Buyerah/Buyerah.js
+++ src/Buyerah/Buyerah.js
@@ -1,71 +1,81 @@
- 
-import React from 'react'
+import React from "react";
 
-import { Container, Row, Col, Button, Image } from 'react-bootstrap';
-
-
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
 
 const Buyerah = () => {
   return (
     <>
-    
-    <div className='About-main-card-1-ah'>
+      <div className="About-main-card-1-ah">
         <h2>Buyer ratings</h2>
-        <div className='satisfied'>
-            <h2>4.8<span>/5.0</span></h2>
-            <h3>98% satisfied buyers</h3>
-            <p>500 ratings</p>
-            <div class="ratings-ah-1-bar  my-ah-class" style={{width:"100%"}}>
+        <div className="satisfied">
+          <h2>
+            4.8<span>/5.0</span>
+          </h2>
+          <h3>98% satisfied buyers</h3>
+          <p>500 ratings</p>
+          <div class="ratings-ah-1-bar  my-ah-class" style={{ width: "100%" }}>
+            <div class="scrllo-bar">
+              <p>5</p>
+              <div class="w3-light-grey w3-tiny">
+                <div
+                  class="w3-container w3-green"
+                  style={{ width: "80%" }}
+                ></div>
+              </div>
+              <p>35</p>
+            </div>
 
-<div class="scrllo-bar">
-    <p>5</p>
-<div class="w3-light-grey w3-tiny">
-    <div class="w3-container w3-green"style={{width:'80%'}}></div>
-  </div>
-  <p>35</p>
-</div>
+            <div class="scrllo-bar">
+              <p>4</p>
+              <div class="w3-light-grey w3-tiny">
+                <div
+                  class="w3-container w3-green"
+                  style={{ width: "60%" }}
+                ></div>
+              </div>
+              <p>5</p>
+            </div>
 
+            <div class="scrllo-bar">
+              <p>3</p>
+              <div class="w3-light-grey w3-tiny">
+                <div
+                  class="w3-container w3-green"
+                  style={{ width: "40%" }}
+                ></div>
+              </div>
+              <p>2</p>
+            </div>
 
-<div class="scrllo-bar">
-    <p>4</p>
-<div class="w3-light-grey w3-tiny">
-    <div class="w3-container w3-green" style={{width:'60%'}}></div>
-  </div>
-  <p>5</p>
-</div>
+            <div class="scrllo-bar">
+              <p>2</p>
+              <div class="w3-light-grey w3-tiny">
+                <div
+                  class="w3-container w3-green"
+                  style={{ width: "25%" }}
+                ></div>
+              </div>
+              <p>2</p>
+            </div>
 
-
-<div class="scrllo-bar">
-    <p>3</p>
-<div class="w3-light-grey w3-tiny">
-    <div class="w3-container w3-green" style={{width:'40%'}}></div>
-  </div>
-  <p>2</p>
-</div>
-
-
-<div class="scrllo-bar">
-    <p>2</p>
-<div class="w3-light-grey w3-tiny">
-    <div class="w3-container w3-green" style={{width:'25%'}}></div>
-  </div>
-  <p>2</p>
-</div>
-
-
-<div class="scrllo-bar">
-    <p>1</p>
-<div class="w3-light-grey w3-tiny">
-    <div class="w3-container w3-green" style={{width:'10%'}}></div>
-  </div>
-  <p>1</p>
-</div>
-</div>
+            <div class="scrllo-bar">
+              <p>1</p>
+              <div class="w3-light-grey w3-tiny">
+                <div
+                  class="w3-container w3-green"
+                  style={{ width: "10%" }}
+                ></div>
+              </div>
+              <p>1</p>
+            </div>
+          </div>{" "}
+          <div className="rate--product--button--container--ah">
             <button>Rate this product</button>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Buyerah
+export default Buyerah;
--- src/Sweatshirtah/Sweatshirtah.js
+++ src/Sweatshirtah/Sweatshirtah.js
@@ -1,40 +1,31 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import Cooperah1 from '../Cooperah/Cooperah1'
-import Cooperah from '../Cooperah/Cooperah'
-import Buyerah from '../Buyerah/Buyerah'
-
-
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import Cooperah1 from "../Cooperah/Cooperah1";
+import Cooperah from "../Cooperah/Cooperah";
+import Buyerah from "../Buyerah/Buyerah";
 
 const Sweatshirtah = () => {
+  return (
+    <>
+      <section className="Sweatshirt-sec">
+        <Container>
+          <div className="Sweatshirt">
+            <Cooperah1 />
 
-    return (
-        <>
-            <section className='Sweatshirt-sec'>
-                
-            <Container>
-                <div className="Sweatshirt">
-                    <Cooperah1/>
+            <div className="Sweatshirt-box Sweatshirt--box--alt">
+              <div className="Sweatshirt-part-1 Sweatshirt--part--alt">
+                <Cooperah />
+              </div>
 
-                    <div className="Sweatshirt-box">
+              <div className="Sweatshirt-part-2 Sweatshirt--part--alt">
+                <Buyerah />
+              </div>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
+};
 
-                    <div className="Sweatshirt-part-1">
-                                 <Cooperah/>
-                    </div>
-
-                    <div className="Sweatshirt-part-2">
-                        <Buyerah/>
-                    </div>
-
-                    </div>
-                </div>
-            </Container>
-            </section>          
-  
-
-        </> 
-    )
-}
-
-export default Sweatshirtah
+export default Sweatshirtah;
