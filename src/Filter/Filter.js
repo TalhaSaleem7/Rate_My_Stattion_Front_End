f39106caa08fd4ba4a89359dd4b249d4867e1983
Ahtisham hussain <147423967+ahteshamniqosa@users.noreply.github.com>
2024-08-09 19:49:23 +0500

    basit2222

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import kabc from "../img/kabc.png";
+import axios from "axios";
+import { baseurl } from "../baseurl";
 
 const Filter = () => {
+  const [stationratings, setStationRatings] = useState([]);
+
+
+  useEffect(() => {
+    // Retrieve user data from local storage
+  
+      // setUser(user)
+
+      fetchUserData();
+    
+  }, []);
+
+
+  const getUserFromLocalStorage = () => {
+    const user = localStorage.getItem('userData');
+    return user ? JSON.parse(user) : null;
+  };
+
+
+  const fetchUserData = async () => {
+
+    const storedUser = getUserFromLocalStorage();
+    const  userId = storedUser.id
+    
+    try {
+      const response = await axios.get(`${baseurl}/getratingsbyuser/${userId}`);
+      const ratings = response.data.ratings;
+
+      console.log("ahtisahm", response.data.ratings);
+      setStationRatings(ratings);
+
+      console.log("hamza", stationratings);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
   return (
     <>
+
       <div class="job-ah-post">
         <h2>
-          My Ratings<span> (19)</span>
+          My Ratings<span> ({stationratings.length})</span>
         </h2>
 
         <div class="job-ah-Filter">
